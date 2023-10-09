e5f2707f90a2fd4a2d05f0ca11d46e901ad61d7d
pranjalipongde09 <pranjalipongde9@gmail.com>
2023-10-09 17:22:38 +0530

    adding ecommerce project

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,149 @@
+import { useEffect, useState } from "react";
+import styles from "./Header.module.scss";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { FaShoppingCart, FaTimes, FaUserCircle } from "react-icons/fa";
+import { HiMenuAlt1 } from "react-icons/hi";
+import { auth } from "../../firebase/config";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { SET_ACTIVE_USER } from "../../redux/slice/authSlice";
+
+const logo = (
+  <div className={styles.logo}>
+    <Link to="/">
+      <h2>
+        e<span>ShoPee</span>.
+      </h2>
+    </Link>
+  </div>
+);
+
+const cart = (
+  <span className={styles.cart}>
+    <Link to="/cart">
+      Cart <FaShoppingCart size={20} />
+      <p>0</p>
+    </Link>
+  </span>
+);
+
+const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
+
+const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [displayName, setDisplayName] = useState("");
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  // monitor currently signed in user
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // console.log(user);
+        // const uid = user.uid;
+        // console.log(user.displayName);
+        setDisplayName(user.displayName);
+
+        dispatch(
+          SET_ACTIVE_USER({
+            email: user.email,
+            userName: user.displayName,
+            userID: user.uid,
+          })
+        );
+      } else {
+        setDisplayName("");
+      }
+    });
+  }, []);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const hideMenu = () => {
+    setShowMenu(false);
+  };
+
+  const logoutUser = () => {
+    signOut(auth)
+      .then(() => {
+        toast.success("Logout Successfully.");
+        navigate("/");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
+
+  return (
+    <header>
+      <div className={styles.header}>
+        {logo}
+        <nav
+          className={
+            showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`
+          }
+        >
+          <div
+            className={
+              showMenu
+                ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`
+                : `${styles["nav-wrapper"]}`
+            }
+            onClick={hideMenu}
+          ></div>
+
+          <ul onClick={hideMenu}>
+            <li className={styles["logo-mobile"]}>
+              {logo}
+              <FaTimes size={22} color="#fff" onClick={hideMenu} />
+            </li>
+            <li>
+              <NavLink to="/" className={activeLink}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" className={activeLink}>
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+
+          <div className={styles["header-right"]} onClick={hideMenu}>
+            <span className={styles.links}>
+              <NavLink to="/login" className={activeLink}>
+                Login
+              </NavLink>
+              <a href="#" style={{ color: "#ff7722" }}>
+                <FaUserCircle size={16} />
+                Hi, {displayName}
+              </a>
+              <NavLink to="/register" className={activeLink}>
+                Register
+              </NavLink>
+              <NavLink to="/order-history" className={activeLink}>
+                My Orders
+              </NavLink>
+              <NavLink to="/" onClick={logoutUser}>
+                LogOut
+              </NavLink>
+            </span>
+            {cart}
+          </div>
+        </nav>
+
+        {/* hamburger menu for responsive view */}
+        <div className={styles["menu-icon"]}>
+          {cart}
+          <HiMenuAlt1 size={28} onClick={toggleMenu} />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
