942318ccc59451ba6cd5c96243b51cb51d3c5824
pranjalipongde09 <pranjalipongde9@gmail.com>
2023-10-10 00:42:47 +0530

    slider component added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,6 +11,7 @@ import {
   REMOVE_ACTIVE_USER,
   SET_ACTIVE_USER,
 } from "../../redux/slice/authSlice";
+import ShowOnLogin, { ShowOnLogout } from "../hiddenLink/hiddenLink";
 
 const logo = (
   <div className={styles.logo}>
@@ -125,22 +126,30 @@ const Header = () => {
 
           <div className={styles["header-right"]} onClick={hideMenu}>
             <span className={styles.links}>
-              <NavLink to="/login" className={activeLink}>
-                Login
-              </NavLink>
-              <a href="#home" style={{ color: "#ff7722" }}>
-                <FaUserCircle size={16} />
-                Hi, {displayName}
-              </a>
-              <NavLink to="/register" className={activeLink}>
-                Register
-              </NavLink>
-              <NavLink to="/order-history" className={activeLink}>
-                My Orders
-              </NavLink>
-              <NavLink to="/" onClick={logoutUser}>
-                LogOut
-              </NavLink>
+              <ShowOnLogout>
+                <NavLink to="/login" className={activeLink}>
+                  Login
+                </NavLink>
+              </ShowOnLogout>
+
+              <ShowOnLogin>
+                <a href="#home" style={{ color: "#ff7722" }}>
+                  <FaUserCircle size={16} />
+                  Hi, {displayName}
+                </a>
+              </ShowOnLogin>
+
+              <ShowOnLogin>
+                <NavLink to="/order-history" className={activeLink}>
+                  My Orders
+                </NavLink>
+              </ShowOnLogin>
+
+              <ShowOnLogin>
+                <NavLink to="/" onClick={logoutUser}>
+                  LogOut
+                </NavLink>
+              </ShowOnLogin>
             </span>
             {cart}
           </div>
---
+++ src/components/hiddenLink/hiddenLink.jsx
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { selectIsLoggedIn } from "../../redux/slice/authSlice";
+
+const ShowOnLogin = ({ children }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  if (isLoggedIn) {
+    return children;
+  }
+  return null;
+};
+
+export const ShowOnLogout = ({ children }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  if (!isLoggedIn) {
+    return children;
+  }
+  return null;
+};
+
+export default ShowOnLogin;
