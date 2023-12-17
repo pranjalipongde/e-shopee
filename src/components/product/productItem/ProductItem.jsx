ac8a1465da412576fe443dd692f5ae6412b507e9
pranjalipongde09 <pranjalipongde9@gmail.com>
2023-12-17 22:49:50 +0530

    add product to cart

--- src/components/product/productItem/ProductItem.jsx
+++ src/components/product/productItem/ProductItem.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import styles from "./ProductItem.module.scss";
 import Card from "../../card/Card";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { ADD_TO_CART } from "../../../redux/slice/cartSlice";
 
 const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
+  const dispatch = useDispatch();
+
   const shortenText = (text, n) => {
     if (text.length > n) {
       const shortenedText = text.substring(0, n).concat("...");
       return shortenedText;
     }
     return text;
+  };
+
+  const addToCart = (product) => {
+    dispatch(ADD_TO_CART(product));
   };
 
   return (
@@ -26,7 +34,12 @@ const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
         </div>
         {!grid && <p className={styles.desc}>{shortenText(desc, 200)}</p>}
 
-        <button className="--btn --btn-danger">Add To Cart</button>
+        <button
+          className="--btn --btn-danger"
+          onClick={() => addToCart(product)}
+        >
+          Add To Cart
+        </button>
       </div>
     </Card>
   );
