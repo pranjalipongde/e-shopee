3a49c3d728072286c4466d7b0cdcb6a24593b106
pranjalipongde09 <pranjalipongde9@gmail.com>
2023-12-19 18:47:41 +0530

    update cart value in header

--- src/components/product/productItem/ProductItem.jsx
+++ src/components/product/productItem/ProductItem.jsx
@@ -3,7 +3,10 @@ import styles from "./ProductItem.module.scss";
 import Card from "../../card/Card";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { ADD_TO_CART } from "../../../redux/slice/cartSlice";
+import {
+  ADD_TO_CART,
+  CALCULATE_TOTAL_QUANTITY,
+} from "../../../redux/slice/cartSlice";
 
 const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
   const dispatch = useDispatch();
@@ -18,6 +21,7 @@ const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
 
   const addToCart = (product) => {
     dispatch(ADD_TO_CART(product));
+    dispatch(CALCULATE_TOTAL_QUANTITY());
   };
 
   return (
